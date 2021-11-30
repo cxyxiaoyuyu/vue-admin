a12dbb7b1b4cb6e5fbafa13796b98d6aa82ce8b2
cxyxiaoyuyu.com <923996381@qq.com>
2021-11-30 20:01:47 +0800

    gzip 压缩

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const compression = require('compression')
 const app = express()
 
+app.use(compression())  // 开启Gzip 压缩
 app.use(express.static('./build'))
 
 app.listen(8091,()=>{
